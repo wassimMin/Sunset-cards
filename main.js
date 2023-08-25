f6a14b81a88ac4508b029b0c45a017164a7f53d1
wassimMin <mohamedwassim4@gmail.com>
2023-08-25 08:44:36 +0100

    sunset

---
+++ main.js
@@ -0,0 +1,14 @@
+const images =  document.querySelectorAll('.image')
+
+images.forEach(image=>{
+    image.addEventListener('click',()=>{
+        removeActiveClasses()
+        image.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    images.forEach(image=>{
+        image.classList.remove('active')
+    })
+}
